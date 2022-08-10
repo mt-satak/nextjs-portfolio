ab452dea89c7ebb8b73082be08322515b3d427ee
mt-satak <sakiware@gmail.com>
2022-08-10 19:39:15 +0900

    mdファイルをNext.jsが読み込むための設定を追加

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // mdファイルをNext.jsで読み込むための設定
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
