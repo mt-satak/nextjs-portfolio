f3a29b149983e97bcf010c3d10533836c15692a6
mt-satak <sakiware@gmail.com>
2022-08-12 10:41:49 +0900

    共通コンポーネントを作成/各ページに反映

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import Layout from "../components/layout"
 
 const Index = () => {
   return (
-    <>
+    <Layout>
       <div>
         <Image src="/images/index-hero.jpg" alt="hero" layout="fill" objectFit="cover" quality={90} />
         <div>
@@ -50,7 +51,7 @@ const Index = () => {
           <Link href="/contact"><a>MakeItHappen!</a></Link>
         </div>
       </div>
-    </>
+    </Layout>
   )
 }
 
