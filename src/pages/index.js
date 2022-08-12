a9604310daa099b423140fec777f82f209850f49
mt-satak <sakiware@gmail.com>
2022-08-12 09:38:29 +0900

    トップページの画像表示処理を追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,55 @@
 import Link from 'next/link'
-// import * as style from "../styles/Index.module.css"
+import Image from 'next/image'
 
 const Index = () => {
   return (
     <>
-      <h1 className={style.h1Text}>こんにちは</h1>
-      <Link href="/contact"><a>こんたくとに移動</a></Link>
+      <div>
+        <Image src="/images/index-hero.jpg" alt="hero" layout="fill" objectFit="cover" quality={90} />
+        <div>
+          <h1>I'm Abe Hiroki!</h1>
+          <h3>JavaScriptDeveloper</h3>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <div>
+            <h2>JavaScriptNerd</h2>
+            <p>LoremIpsumissimplydummytextoftheprintingandtypesettingindustry.LoremIpsumhasbeentheindustry'sstandarddummytexteversincethe1500s,whenanunknownprintertookagalleyoftypeandscrambledittomakeatypespecimenbook.Ithassurvivednotonlyfivecenturies,butalsotheleapintoelectronictypesetting,remainingessentiallyunchanged.Itwaspopularisedinthe1960swiththereleaseofLetrasetsheetscontainingLoremIpsumpassages,andmorerecentlywithdesktoppublishingsoftwarelikeAldusPageMakerincludingversionsofLoremIpsum.</p>
+          </div>
+          <Image src="/images/profile.jpg" alt="hero" height={1195} width={1000} quality={90} />
+        </div>
+
+        <div>
+          <h2>Skills</h2>
+          <div>
+            <div>
+              <img src="/images/javascript.svg" alt="javascript"/>
+              <span>JavaScript/10years</span>
+            </div>
+            
+            <div>
+              <img src="/images/react.svg" alt="react"/>
+              <span>React/5years</span>
+            </div>
+
+            <div>
+              <img src="/images/gatsby.svg" alt="gatsby"/>
+              <span>Gatsby/3years</span>
+            </div>
+
+            <div>
+              <img src="/images/next.svg" alt="next"/>
+              <span>Next.JS/3years</span>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <Link href="/contact"><a>MakeItHappen!</a></Link>
+        </div>
+      </div>
     </>
   )
 }
